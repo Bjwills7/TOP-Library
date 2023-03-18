1d10f3f818c6d1398af88526fe5fdf8d5181bfbb
Bjwills7 <bjwills7@hotmail.com>
2023-03-17 21:07:38 -0500

    Add buttons and content card Organize elements with grid and flex Style text content

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 let myLibrary = [];
 
+let newBookButton = document.querySelector(".add-book");
+
+addBook("Beeg Yoshi", "donkey", 1, "has been read");
+
 function Book(title, author, pages, read) {
   this.title = title;
   this.author = author;
@@ -14,3 +18,8 @@ function addBook(title, author, pages, read) {
   let newBook = new Book(title, author, pages, read);
   myLibrary.push(newBook);
 }
+
+// remove current card?
+// add next card?
+// or just change the content?
+function changeCard() {}
