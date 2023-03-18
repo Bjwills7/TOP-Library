67dde23459c8c2db4bee8f4d4b7925d56227f90a
Bjwills7 <bjwills7@hotmail.com>
2023-03-17 21:51:36 -0500

    Add function to change text content on card Add event listeners that change text content to match the current book object

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
 let myLibrary = [];
+let iterator = 0;
 
+// buttons
 let newBookButton = document.querySelector(".add-book");
+let changeBookLeft = document.querySelector(".left");
+let changeBookRight = document.querySelector(".right");
+
+// event listeners
+changeBookLeft.addEventListener("click", () => {
+  iterator -= 1;
+  changeCard(iterator);
+});
+changeBookRight.addEventListener("click", () => {
+  iterator += 1;
+  changeCard(iterator);
+});
+
+let titleP = document.querySelector(".title");
+let authorP = document.querySelector(".author");
+let pagesP = document.querySelector(".pages");
+let readP = document.querySelector(".read");
 
 addBook("Beeg Yoshi", "donkey", 1, "has been read");
 
@@ -17,9 +36,18 @@ function Book(title, author, pages, read) {
 function addBook(title, author, pages, read) {
   let newBook = new Book(title, author, pages, read);
   myLibrary.push(newBook);
+  changeCard(myLibrary.indexOf(newBook));
 }
 
-// remove current card?
-// add next card?
-// or just change the content?
-function changeCard() {}
+function changeCard(i) {
+  if (myLibrary[i] === undefined) {
+    return;
+  }
+  titleP.textContent = myLibrary[i].title;
+  authorP.textContent = myLibrary[i].author;
+  pagesP.textContent = myLibrary[i].pages;
+  readP.textContent = myLibrary[i].read;
+}
+
+// remove card and replace it with form?
+function openForm() {}
