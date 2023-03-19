0b0a1f18bcb54a376e3c30fa49d5851ce04b2148
Bjwills7 <bjwills7@hotmail.com>
2023-03-18 21:13:57 -0500

    Finish new book and book cycling functionality Remove parameter from changeCard function

--- script.js
+++ script.js
@@ -1,19 +1,58 @@
 let myLibrary = [];
 let iterator = 0;
 
-// buttons
-let newBookButton = document.querySelector(".add-book");
-let changeBookLeft = document.querySelector(".left");
-let changeBookRight = document.querySelector(".right");
+// displays
+const mainContainer = document.querySelector(".container");
+const formModal = document.querySelector(".modal");
 
-// event listeners
+// buttons
+const newBookButton = document.querySelector(".add-book");
+const changeBookLeft = document.querySelector(".left");
+const changeBookRight = document.querySelector(".right");
+const formCancel = document.querySelector(".form-cancel");
+const formSubmit = document.querySelector(".form-submit");
+
+// form inputs
+const formTitle = document.querySelector(".form-title");
+const formAuthor = document.querySelector(".form-author");
+const formPages = document.querySelector(".form-pages");
+const formRead = document.querySelector(".form-read");
+
 changeBookLeft.addEventListener("click", () => {
+  if (myLibrary[iterator - 1] === undefined) {
+    iterator = myLibrary.length - 1;
+    changeCard();
+    return;
+  }
   iterator -= 1;
-  changeCard(iterator);
+  console.log(iterator);
+  changeCard();
 });
 changeBookRight.addEventListener("click", () => {
+  if (myLibrary[iterator + 1] === undefined) {
+    iterator = 0;
+    changeCard();
+    return;
+  }
   iterator += 1;
-  changeCard(iterator);
+  console.log(iterator);
+  changeCard();
+});
+newBookButton.addEventListener("click", () => {
+  openForm();
+});
+formCancel.addEventListener("click", () => {
+  closeForm();
+});
+formSubmit.addEventListener("click", (e) => {
+  e.preventDefault();
+  addBook(
+    formTitle.value,
+    formAuthor.value,
+    formPages.value,
+    formRead.checked === true ? "Read" : "Not read"
+  );
+  closeForm();
 });
 
 let titleP = document.querySelector(".title");
@@ -36,18 +75,28 @@ function Book(title, author, pages, read) {
 function addBook(title, author, pages, read) {
   let newBook = new Book(title, author, pages, read);
   myLibrary.push(newBook);
-  changeCard(myLibrary.indexOf(newBook));
+  iterator = myLibrary.indexOf(newBook);
+  changeCard();
 }
 
-function changeCard(i) {
-  if (myLibrary[i] === undefined) {
-    return;
-  }
-  titleP.textContent = myLibrary[i].title;
-  authorP.textContent = myLibrary[i].author;
-  pagesP.textContent = myLibrary[i].pages;
-  readP.textContent = myLibrary[i].read;
+function changeCard() {
+  titleP.textContent = myLibrary[iterator].title;
+  authorP.textContent = myLibrary[iterator].author;
+  pagesP.textContent = myLibrary[iterator].pages;
+  readP.textContent = myLibrary[iterator].read;
 }
 
-// remove card and replace it with form?
-function openForm() {}
+function openForm() {
+  mainContainer.style.display = "none";
+  formModal.style.display = "grid";
+}
+
+function closeForm() {
+  formModal.style.display = "none";
+  mainContainer.style.display = "grid";
+}
+
+function submitForm() {
+  addBook();
+  closeForm();
+}
