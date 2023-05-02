4c8e6e4895f61db6a8924320ca91ef69b946ed37
Bjwills7 <bjwills7@hotmail.com>
2023-05-01 20:08:03 -0500

    Refactor everything into classes

--- script.js
+++ script.js
@@ -1,159 +1,205 @@
-let myLibrary = [];
+class Library {
+  constructor() {
+    this.bookCase = [];
+  }
+
+  addBook() {
+    let newBook = new Book(
+      inputs.title.value,
+      inputs.author.value,
+      inputs.pages.value,
+      inputs.read.checked
+    );
+    this.bookCase.push(newBook);
+    iterator = this.bookCase.indexOf(newBook);
+  }
+}
+
+class Book {
+  constructor(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+  }
+
+  toggleReadBool() {
+    this.read ? (this.read = false) : (this.read = true);
+  }
+}
+
+class UserInterface {
+  constructor() {}
+}
+
+class Display extends UserInterface {
+  constructor() {
+    super();
+    this.formModal = document.querySelector(".modal");
+    this.mainContainer = document.querySelector(".container");
+    this.cardContainer = document.querySelector(".main");
+  }
+
+  openForm() {
+    this.formModal.style.display = "grid";
+  }
+
+  closeForm() {
+    this.formModal.style.display = "none";
+  }
+}
+
+class Card extends Display {
+  constructor() {
+    super();
+    this.titleP = document.querySelector(".title");
+    this.authorP = document.querySelector(".author");
+    this.pagesP = document.querySelector(".pages");
+    this.readP = document.querySelector(".read");
+  }
+
+  setActiveElements(target) {
+    this.readP = target.previousElementSibling;
+    this.pagesP = this.readP.previousElementSibling;
+    this.authorP = this.pagesP.previousElementSibling;
+    this.titleP = this.authorP.previousElementSibling;
+  }
+
+  changeCard(index) {
+    this.titleP.textContent = library.bookCase[index].title;
+    this.authorP.textContent = `By: ${library.bookCase[index].author}`;
+    this.pagesP.textContent = `Pages: ${library.bookCase[index].pages}`;
+    if (library.bookCase[index].read) {
+      this.readP.textContent = "Read";
+    } else {
+      this.readP.textContent = "Unread";
+    }
+  }
+
+  createCard() {
+    let newCard = display.cardContainer.appendChild(
+      document.createElement("div")
+    );
+    let newTitleP = newCard.appendChild(document.createElement("p"));
+    let newAuthorP = newCard.appendChild(document.createElement("p"));
+    let newPagesP = newCard.appendChild(document.createElement("p"));
+    let newReadP = newCard.appendChild(document.createElement("p"));
+    let newMarkRead = newCard.appendChild(document.createElement("input"));
+    let newMarkReadL = newCard.appendChild(document.createElement("label"));
+    let newRemoveBookButton = newCard.appendChild(
+      document.createElement("button")
+    );
+    newCard.setAttribute("class", "card");
+    newTitleP.setAttribute("class", "title");
+    newAuthorP.setAttribute("class", "author");
+    newPagesP.setAttribute("class", "pages");
+    newReadP.setAttribute("class", "read");
+    newMarkRead.setAttribute("class", "card-is-read");
+    newMarkRead.setAttribute("type", "checkbox");
+    newMarkRead.setAttribute("data-index", iterator);
+    newMarkRead.setAttribute("id", `cb${iterator}`);
+    newMarkReadL.setAttribute("for", `cb${iterator}`);
+    newMarkReadL.textContent = "✔";
+    newRemoveBookButton.setAttribute("class", "remove-book");
+    newRemoveBookButton.setAttribute("data-index", iterator);
+    newRemoveBookButton.textContent = "✖";
+    this.setActiveElements(newMarkRead);
+    newMarkRead.addEventListener("click", (e) => {
+      this.setActiveElements(e.target); // needed to ensure the appropriate checkbox is changed
+      library.bookCase[e.target.dataset.index].toggleReadBool();
+      card.changeCard(e.target.dataset.index);
+    });
+    newRemoveBookButton.addEventListener("click", (e) => {
+      library.bookCase.forEach((book) => {
+        if (library.bookCase.indexOf(book) > e.target.dataset.index) {
+          display.cardContainer.children[
+            library.bookCase.indexOf(book)
+          ].querySelector(".card-is-read").dataset.index -= 1;
+          display.cardContainer.children[
+            library.bookCase.indexOf(book)
+          ].querySelector(".remove-book").dataset.index -= 1;
+        }
+      });
+      library.bookCase.splice(e.target.dataset.index, 1);
+      e.target.parentElement.remove();
+    });
+  }
+}
+
+class Buttons extends UserInterface {
+  constructor() {
+    super();
+    this.newBookButton = document.querySelector(".add-book");
+    this.changeBookLeft = document.querySelector(".left");
+    this.changeBookRight = document.querySelector(".right");
+    this.formCancel = document.querySelector(".form-cancel");
+    this.formSubmit = document.querySelector(".form-submit");
+    this.markRead = document.querySelector(".card-is-read");
+    this.removeBookButton = document.querySelector(".remove-book");
+  }
+
+  initListeners() {
+    buttons.newBookButton.addEventListener("click", () => {
+      inputs.read.checked = false;
+      display.openForm();
+    });
+
+    buttons.formCancel.addEventListener("click", (e) => {
+      e.preventDefault();
+      display.closeForm();
+    });
+
+    buttons.formSubmit.addEventListener("click", (e) => {
+      e.preventDefault();
+      library.addBook(
+        inputs.title.value,
+        inputs.author.value,
+        inputs.pages.value,
+        inputs.read.checked
+      );
+      card.createCard();
+      card.changeCard(iterator);
+      if (library.bookCase[iterator].read) {
+        document.querySelector(`#cb${iterator}`).checked = true;
+      }
+      if (cards[0].lastChild.classList.value === "add-book") {
+        cards[0].remove();
+      }
+      display.closeForm();
+    });
+  }
+}
+
+class Inputs extends UserInterface {
+  constructor() {
+    super();
+    this.title = document.querySelector(".form-title");
+    this.author = document.querySelector(".form-author");
+    this.pages = document.querySelector(".form-pages");
+    this.read = document.querySelector(".form-read");
+  }
+}
+
+const library = new Library();
+const book = new Book();
+const display = new Display();
+const card = new Card();
+const buttons = new Buttons();
+const inputs = new Inputs();
+
+buttons.initListeners();
+
 let iterator = 0;
 
 window.onload = () => {
   createPlaceholder();
 };
 
-// displays
-const mainContainer = document.querySelector(".container");
-const formModal = document.querySelector(".modal");
-const cardContainer = document.querySelector(".main");
-
-// buttons
-const newBookButton = document.querySelector(".add-book");
-const changeBookLeft = document.querySelector(".left");
-const changeBookRight = document.querySelector(".right");
-const formCancel = document.querySelector(".form-cancel");
-const formSubmit = document.querySelector(".form-submit");
-const markRead = document.querySelector(".card-is-read");
-let markReadAll = document.querySelectorAll(".card-is-read");
-const removeBookButton = document.querySelector(".remove-book");
-
-// form inputs
-const formTitle = document.querySelector(".form-title");
-const formAuthor = document.querySelector(".form-author");
-const formPages = document.querySelector(".form-pages");
-const formRead = document.querySelector(".form-read");
-
-newBookButton.addEventListener("click", () => {
-  formRead.checked = false;
-  openForm();
-});
-formCancel.addEventListener("click", (e) => {
-  e.preventDefault();
-  closeForm();
-});
-formSubmit.addEventListener("click", (e) => {
-  e.preventDefault();
-  addBook(formTitle.value, formAuthor.value, formPages.value, formRead.checked);
-  createCard();
-  changeCard(iterator);
-  if (myLibrary[iterator].read) {
-    document.querySelector(`#cb${iterator}`).checked = true;
-  }
-  if (cards[0].lastChild.classList.value === "add-book") {
-    cards[0].remove();
-  }
-  closeForm();
-});
-
-cardContainer.addEventListener("change", () => {
-  if (myLibrary.length < 1) {
-    createPlaceholder();
-  }
-});
-
-let titleP = document.querySelector(".title");
-let authorP = document.querySelector(".author");
-let pagesP = document.querySelector(".pages");
-let readP = document.querySelector(".read");
-
-function Book(title, author, pages, read) {
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  this.read = read;
-}
-
-function addBook(title, author, pages, read) {
-  let newBook = new Book(title, author, pages, read);
-  myLibrary.push(newBook);
-  iterator = myLibrary.indexOf(newBook);
-}
-
-function changeCard(index) {
-  titleP.textContent = myLibrary[index].title;
-  authorP.textContent = `By: ${myLibrary[index].author}`;
-  pagesP.textContent = `Pages: ${myLibrary[index].pages}`;
-  if (myLibrary[index].read) {
-    readP.textContent = "Read";
-  } else {
-    readP.textContent = "Unread";
-  }
-}
-
-function openForm() {
-  formModal.style.display = "grid";
-}
-
-function closeForm() {
-  formModal.style.display = "none";
-}
-
-Book.prototype.toggleReadBool = function () {
-  if (this.read) {
-    this.read = false;
-  } else {
-    this.read = true;
-  }
-};
-
-// branch refactor stuff
-function createCard() {
-  let newCard = cardContainer.appendChild(document.createElement("div"));
-  let newTitleP = newCard.appendChild(document.createElement("p"));
-  let newAuthorP = newCard.appendChild(document.createElement("p"));
-  let newPagesP = newCard.appendChild(document.createElement("p"));
-  let newReadP = newCard.appendChild(document.createElement("p"));
-  let newMarkRead = newCard.appendChild(document.createElement("input"));
-  let newMarkReadL = newCard.appendChild(document.createElement("label"));
-  let newRemoveBookButton = newCard.appendChild(
-    document.createElement("button")
-  );
-  newCard.setAttribute("class", "card");
-  newTitleP.setAttribute("class", "title");
-  newAuthorP.setAttribute("class", "author");
-  newPagesP.setAttribute("class", "pages");
-  newReadP.setAttribute("class", "read");
-  newMarkRead.setAttribute("class", "card-is-read");
-  newMarkRead.setAttribute("type", "checkbox");
-  newMarkRead.setAttribute("data-index", iterator);
-  newMarkRead.setAttribute("id", `cb${iterator}`);
-  newMarkReadL.setAttribute("for", `cb${iterator}`);
-  newMarkReadL.textContent = "✔";
-  newRemoveBookButton.setAttribute("class", "remove-book");
-  newRemoveBookButton.setAttribute("data-index", iterator);
-  newRemoveBookButton.textContent = "✖";
-  titleP = newTitleP; // these need to be redefined in the event listener below
-  authorP = newAuthorP;
-  pagesP = newPagesP;
-  readP = newReadP;
-  markReadAll = document.querySelectorAll(".card-is-read");
-  newMarkRead.addEventListener("click", (e) => {
-    setActiveElements(e);
-    myLibrary[e.target.dataset.index].toggleReadBool();
-    changeCard(e.target.dataset.index);
-  });
-  newRemoveBookButton.addEventListener("click", (e) => {
-    myLibrary.forEach((book) => {
-      if (myLibrary.indexOf(book) > e.target.dataset.index) {
-        cardContainer.children[myLibrary.indexOf(book)].querySelector(
-          ".card-is-read"
-        ).dataset.index -= 1;
-        cardContainer.children[myLibrary.indexOf(book)].querySelector(
-          ".remove-book"
-        ).dataset.index -= 1;
-      }
-    });
-    myLibrary.splice(e.target.dataset.index, 1);
-    e.target.parentElement.remove();
-  });
-}
-
-// create placeholder card on page load and when library.length === 0
+// called on page load and when library.length === 0
 function createPlaceholder() {
-  let newCard = cardContainer.appendChild(document.createElement("div"));
+  let newCard = display.cardContainer.appendChild(
+    document.createElement("div")
+  );
   let title = newCard.appendChild(document.createElement("p"));
   let addCardButton = newCard.appendChild(document.createElement("button"));
   newCard.setAttribute("class", "card");
@@ -162,22 +208,16 @@ function createPlaceholder() {
   addCardButton.textContent = "+";
   addCardButton.setAttribute("class", "add-book");
   addCardButton.addEventListener("click", () => {
-    openForm();
+    display.openForm();
   });
 }
 
+// checks for DOM changes on cards and creates placeholder if no books are present
 let cards = document.querySelectorAll(".card");
 const mutationObserver = new MutationObserver((entries) => {
   cards = document.querySelectorAll(".card");
-  if (myLibrary.length < 1 && cards.length < 1) {
+  if (library.bookCase.length < 1 && cards.length < 1) {
     createPlaceholder();
   }
 });
-mutationObserver.observe(cardContainer, { childList: true });
-
-function setActiveElements(e) {
-  readP = e.target.previousElementSibling;
-  pagesP = readP.previousElementSibling;
-  authorP = pagesP.previousElementSibling;
-  titleP = authorP.previousElementSibling;
-}
+mutationObserver.observe(display.cardContainer, { childList: true });
